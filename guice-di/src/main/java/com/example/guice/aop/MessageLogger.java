c50cfb42a8ea5b73530adb487150ef015e9e1049
Amh <abaams78@gmail.com>
2020-03-14 12:51:25 -0500

    guice di practice 2

--- guice-di/src/main/java/com/example/guice/aop/MessageLogger.java
+++ guice-di/src/main/java/com/example/guice/aop/MessageLogger.java
@@ -1,4 +1,23 @@
 package com.example.guice.aop;
 
-public class MessageLogger {
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author baeldung
+ */
+public class MessageLogger implements MethodInterceptor {
+
+
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        Object[] objectArray = invocation.getArguments();
+        int i = 0;
+        for (Object object : objectArray) {
+            Logger.getAnonymousLogger().info("Sending message: " + object.toString());
+        }
+        return invocation.proceed();
+    }
 }
--- guice-di/src/main/java/com/example/guice/aop/MessageSentLoggable.java
+++ guice-di/src/main/java/com/example/guice/aop/MessageSentLoggable.java
@@ -1,4 +1,16 @@
 package com.example.guice.aop;
 
-public class MessageSentLoggable {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author baeldung
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface MessageSentLoggable {
+
 }
