48a7e98d47f84a36a9d5507aedc69e620d7235a8
Amh <abaams78@gmail.com>
2020-03-14 12:52:05 -0500

    guice di practice 4

--- guice-di/src/main/java/com/example/guice/module/BasicModule.java
+++ guice-di/src/main/java/com/example/guice/module/BasicModule.java
@@ -1,4 +1,29 @@
 package com.example.guice.module;
 
-public class BasicModule {
+import com.example.guice.*;
+import com.google.inject.AbstractModule;
+import com.google.inject.name.Names;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class BasicModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        try {
+            bind(Communication.class).toConstructor(Communication.class.getConstructor(Boolean.class));
+            bind(Boolean.class).toInstance(true);
+        } catch (NoSuchMethodException ex) {
+            Logger.getLogger(com.example.guice.binding.BasicModule.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex) {
+            Logger.getLogger(com.example.guice.binding.BasicModule.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        bind(DefaultCommunicator.class).annotatedWith(Names.named("AnotherCommunicator")).to(DefaultCommunicator.class).asEagerSingleton();
+
+        bind(CommunicationMode.class).annotatedWith(Names.named("IMComms")).to(IMCommunicationMode.class);
+        bind(CommunicationMode.class).annotatedWith(Names.named("EmailComms")).to(EmailCommunicationMode.class);
+        bind(CommunicationMode.class).annotatedWith(Names.named("SMSComms")).to(SMSCommunicationMode.class);
+    }
+
 }
