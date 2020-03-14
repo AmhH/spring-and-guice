9d0d7f3249f0d59a467955a681c1fe40c97443e0
Amh <abaams78@gmail.com>
2020-03-14 12:51:45 -0500

    guice di practice 3

--- guice-di/src/main/java/com/example/guice/binding/AOPModule.java
+++ guice-di/src/main/java/com/example/guice/binding/AOPModule.java
@@ -1,4 +1,22 @@
 package com.example.guice.binding;
 
-public class AOPModule {
+import com.example.guice.aop.MessageLogger;
+import com.example.guice.aop.MessageSentLoggable;
+import com.google.inject.AbstractModule;
+import com.google.inject.matcher.Matchers;
+
+/**
+ *
+ * @author baeldung
+ */
+public class AOPModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        bindInterceptor(Matchers.any(),
+                Matchers.annotatedWith(MessageSentLoggable.class),
+                new MessageLogger()
+        );
+    }
+
 }
